6303388a7f3ef3e642626421f5245be0ab1fd52d
Carlos <silva.carloseduardo.ds@gmail.com>
2025-05-14 19:39:28 -0300

    feat: add temporary splash screen with placeholder logo

--- app/src/main/java/com/example/codevs/MainActivity.kt
+++ app/src/main/java/com/example/codevs/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.codevs
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +16,54 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.codevs.ui.theme.CodevsTheme
 
 class MainActivity : ComponentActivity() {
+    private var setKeepOnScreenCondition = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
+        installSplashScreen().apply {
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.4f,
+                    0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.4f,
+                    0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+            }
+
+
+            setKeepOnScreenCondition {
+                setKeepOnScreenCondition
+            }
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            setKeepOnScreenCondition = false
+        }, 3000)
+
         setContent {
             CodevsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
